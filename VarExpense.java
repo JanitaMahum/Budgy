6770ab54776ce5171b4c6c65e232a0d113a61853
Michaela Gilchrist <59849464+mjgilchrist@users.noreply.github.com>
2020-02-27 11:47:16 -0700

    Update VarExpense.java

--- VarExpense.java
+++ VarExpense.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
 
 public class VarExpense {
+	
+//private ArrayList<Double> varExpense;
 
+public ArrayList<Double> getVarExpense() 
+{
+	return varExpense;
+}
+//move to expense class
+public void setVarExpense(ArrayList<Double> varExpense) {
+	this.varExpense = varExpense;
+}
+
+//move to expense class
+public ArrayList<Double> addVarExpense(double varExpense)
+{
+	this.varExpense.add(varExpense);
+	
+	return this.varExpense;
+}
+//move to expense class
+public ArrayList<Double> delVarExpense(double varExpense)
+{
+	this.varExpense.remove(varExpense);
+		
+	return this.varExpense;
+	
+}
+//move to expense class
+public double totalVarExpense(ArrayList<Double> varExpense)
+{
+	double totalVarExpense = 0;
+	
+	for(int index = 0; index < varExpense.size(); index++) 
+	{totalVarExpense += varExpense.get(index);}
+	
+	return totalVarExpense;
+}
 }
