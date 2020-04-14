c9a7549637377a20942ee4b09607dc76c874221e
ChimzyTasie7 <60757295+ChimzyTasie7@users.noreply.github.com>
2020-04-13 20:26:50 -0600

    Add files via upload

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,9 +2,16 @@ package application;
 	
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import application.User;
+import javax.swing.JTextField;
+
+import backend.User;
+import backend.SavingCalculator;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -12,16 +19,20 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
@@ -30,19 +41,54 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
+/** This is the main class and it runs the application
 
+ */
 public class Main extends Application {
 	
 	int counter = 0;
+	User newUser;
+	SavingCalculator newSavCalc;
+	Label newLabel;
+	Label finalLabel;
+	ArrayList<Double> expenses;
+	TextField newTextField;
+	double firstExp;
+	double addExp;
+	double totalExp;
 	
-	public Main() {
-		
+	/**
+	* This is the Main method which intializes the variables.
+	* @param Nothing.
+	* @return Nothing.
+	*/
+	public Main()
+	{
+		newUser = new User();
+		newSavCalc = new SavingCalculator();
+		newLabel = new Label();
+		expenses = newUser.getExpense();
+		newTextField = new TextField();
+		this.counter = 0;
 	}
+	
+	/**
+	* This is the Main method constructor which takes arguements.
+	* @param String aName, double anIncome, ArrayList<Double> someExpenses, double someSavings.
+	* @return Nothing.
+	*/
+	public Main(String aName, double anIncome, ArrayList<Double> someExpenses, double someSavings)
+	{
+		newUser = new User(aName, anIncome, someExpenses);
+		newSavCalc = new SavingCalculator(aName, anIncome, someExpenses, someSavings);
+	}
+	
+
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
+	
 		
-		User newUser = new User();
 			FirstController n = new FirstController();
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("First.fxml"));			
 			Scene scene = new Scene(root,600,500);
@@ -57,129 +103,129 @@ public class Main extends Application {
 			p.setHgap(10);
 			p.setVgap(10);
 			p.setPadding(new Insets(25, 25, 25, 25));
-			Text scenetitle = new Text("Pleae enter the following information:");
+			Text scenetitle = new Text("Welcome to Budgy!");
 			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 			p.add(scenetitle, 0, 0, 2, 1);
+			Text sceneSubTitle = new Text("Please enter the following:");
+			sceneSubTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+			p.add(sceneSubTitle, 0, 1, 2, 1);
 			
-			 Label nameLabel = new Label("Name: ");
-		     	p.add(nameLabel, 0, 1);
+			try {
+				
+				Label money = new Label("                                        $");
+				Label nameLabel = new Label("Name: ");
+		     	p.add(nameLabel, 0, 2);
 		     	TextField nameText = new TextField();
-		     	p.add(nameText, 1, 1);
-		     	
-		     	
+		     	nameText.setText("Anonymous");
+		     	newUser.setName("Anonymous");
+		     	p.add(nameText, 1, 2);
+
 		        
-		     Label income = new Label("Income: ");
-		     	p.add(income, 0, 2);
+		     Label income = new Label("Income:");
+		     	p.add(income, 0, 3);
+		     	p.add(money, 0, 3);
 		        TextField incomeText = new TextField();
-		        p.add(incomeText, 1, 2);
-		        
+		        incomeText.setText("0.0");
+		        newUser.setIncome(0.0);
+		        p.add(incomeText, 1, 3);
+	        
 		
 		        Button addExp=new Button("+");
-		        p.add(addExp, 3, 3);
-		        Label expense = new Label("Add Expense: ");
-		        p.add(expense, 0, 3);
-		        TextField expenseText = new TextField();
-		        p.add(expenseText, 1, 3);
+		        p.add(addExp, 3, 4);
+		        Label expense = new Label("Add Expense:");
+		        Label money2 = new Label("                                        $");
+		        p.add(expense, 0, 4);
+		        p.add(money2, 0, 4);
+		     
+		        ArrayList<TextField> textFields = new ArrayList<TextField>();
 		        
-		        TextField newExpText = new TextField();
+		        for (int index = 0; index < 100; index++)
+		        {
+		        	TextField tF = new TextField(String.valueOf(index+1));
+		        	tF.setId("TF"+String.valueOf(index+1));
+		        	tF.setText("0.0");
+		        	textFields.add(tF);
+		        	System.out.println(tF);	
+		        }
 		        
+		        
+		        p.add(textFields.get(this.counter), 1, 4);
+		        newUser.addExpense(0.0);
+
 		        addExp.setOnAction(e -> 
 		        {
-		        	
-		    
-		            p.add(newExpText, 1, 4);
-		            Button addExp2=new Button("+");
-			        p.add(addExp2, 3, 4);
-			        Label expense2 = new Label("Add Expense: ");
-			        p.add(expense2, 0, 4);
-			        TextField expenseText2 = new TextField();
-			        p.add(expenseText2, 1, 4);
-			        
-			        TextField newExpText2 = new TextField();
-			        
-			        addExp2.setOnAction(d -> 
-			        {
-			        
-			    
-			            p.add(newExpText2, 1, 5);
-			        Button addExp3=new Button("+");
-		        p.add(addExp3, 3, 5);
-		        Label expense3 = new Label("Add Expense: ");
-		        p.add(expense3, 0, 5);
-		        TextField expenseText3 = new TextField();
-		        p.add(expenseText3, 1, 5);
-		        
-		        TextField newExpText3 = new TextField();
-		        
-		        addExp3.setOnAction(f -> 
-		        {
-		        	
-		            p.add(newExpText3, 1, 6);
-		            Button addExp4=new Button("+");
-		        p.add(addExp4, 3, 6);
-		        Label expense4 = new Label("Add Expense: ");
-		        p.add(expense4, 0, 6);
-		        TextField expenseText4 = new TextField();
-		        p.add(expenseText4, 1, 6);
-		        
-		        TextField newExpText4 = new TextField();
-		        
-		        addExp4.setOnAction(g -> 
-		        {
-		        	
-		            p.add(newExpText4, 1, 7);
-		            Button addExp5=new Button("+");
-		        p.add(addExp5, 3, 7);
-		        Label expense5 = new Label("Add Expense: ");
-		        p.add(expense5, 0, 7);
-		        TextField expenseText5 = new TextField();
-		        p.add(expenseText5, 1, 7);
-		        
-		        TextField newExpText5 = new TextField();
-		     
-		        addExp5.setOnAction(h -> 
-		        {
-		  
-		        		});
+		        	System.out.println(textFields.get(this.counter).getText());
+		        	newUser.addExpense(Double.valueOf(textFields.get(this.counter).getText()));
+		        	this.counter++;
+		        	p.add(textFields.get(this.counter), 1, 4);
+		        	System.out.println(newUser.getExpense());
 
 		        });
 
+		        Button calcSav = new Button("Calculate Savings");
+		        p.add(calcSav, 1, 5);
+				GridPane q = new GridPane();
+		        //AnchorPane root2 = (AnchorPane)FXMLLoader.load(getClass().getResource("FinalScreen.fxml"));			
+				Scene scene3 = new Scene(q,600,500);
+				
+		        calcSav.setOnAction(e -> 
+		        
+		        {	
+		        	newUser.setName(nameText.getText());
+		        	newUser.setIncome(Double.valueOf(incomeText.getText()));
+		        	newUser.addExpense(Double.valueOf(textFields.get(this.counter).getText()));
+		        	
+		        	primaryStage.setScene(scene3);
+
+					q.setAlignment(Pos.CENTER);
+					q.setHgap(10);
+					q.setVgap(10);
+					q.setPadding(new Insets(25, 25, 25, 25));
+					Text results = new Text("Results");
+					results.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+					q.add(results, 0, 0, 2, 1);
+					Text scene3Title = new Text("Hi " + newUser.getName() + "...");
+					scene3Title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					q.add(scene3Title, 0, 2, 2, 1);
+					Text scene3SubTitle = new Text("...you saved..." );
+					scene3SubTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					q.add(scene3SubTitle, 0, 3, 2, 1);
+					Text scene3SubTitle2 = new Text("$" + Double.toString(newSavCalc.totalSavings(newUser.getExpense(), newUser.getIncome())));
+					scene3SubTitle2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					q.add(scene3SubTitle2, 0, 4, 2, 1);
+					Text scene3SubTitle3 = new Text("...this month");
+					scene3SubTitle3.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					q.add(scene3SubTitle3, 0, 5, 2, 1);});
+		        
+		        Button done = new Button("Done");
+		        q.add(done, 1, 8);
+		        
+		        done.setOnAction(e -> 
+		        {
+		        	  System.exit(0);
+		        	 
 		        });
 
-			        });
-
-		        });
- 
-			Button finish=new Button("Finish");
-			p.add(finish, 1, 10);
-			
-			Label endLabel = new Label("Hi " + newUser.getName() + " you have saved a total of " + newUser.getSavings() + " this month.");
-			Button done = new Button("Done");
-			
-			finish.setOnAction(e -> 
-			{
-		     newUser.setName(nameText.getText());
-			 newUser.setIncome(Double.valueOf(incomeText.getText())); 
-			 newUser.addExpense(Double.valueOf(expenseText.getText()));
-		     p.add(endLabel, 0, counter+7);
-		     p.add(done, 1, counter+8);
-			});
-		
-			
-			done.setOnAction(e -> 
-			{
-				primaryStage.setScene(scene);
-			});
-			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 
 
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+
 	}
-				
+	
+
 
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) 
+	{
 		launch(args);
 	}
 }
